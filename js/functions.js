6ffa08f30bd340dbd19a28f1cce8069f1901cf05
Владимир <10nva63@gmail.com>
2024-10-20 22:42:09 +0300

    вторая  функция

--- js/functions.js
+++ js/functions.js
@@ -1,13 +1,22 @@
+/* проверка длины строки */
 
 const detLengthString = (string, maxLength) => string.length <= maxLength;
 
 console.log(detLengthString('проверяемая строка', 18));
 
+/* проверка  является ли строка палиндромом */
 
-function detPalindrome (str) {
-  str.replaceAll(' ', ' ');
+const text = 'Лёша на полке клопа нашёл';
+const str = text.replaceAll(' ', '') .toLowerCase();
+
+let nevStr = '';
+
+for(let i = str.length - 1; i >= 0; i--) {
+  nevStr += str[i] ;
 }
 
-const str = 'Лёша на полке клопа нашёл';
+const detPalindrome = () =>(nevStr === str);
 
-console.log(str.replaceAll(' ', ''));
+console.log(detPalindrome(nevStr));
+
+
