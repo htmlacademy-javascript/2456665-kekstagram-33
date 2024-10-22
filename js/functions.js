818efb68899a4954d94591fa17f27e3679987ee4
Владимир <10nva63@gmail.com>
2024-10-22 14:34:08 +0300

    Исправление замечаний. Дополнительное задание

--- js/functions.js
+++ js/functions.js
@@ -1,21 +1,33 @@
 /* проверка длины строки */
 
 const detLengthString = (string, maxLength) => string.length <= maxLength;
-
-detLengthString();
+//console.log(detLengthString('aaaaaaaaaa', 3));
 
 
 /* проверка  является ли строка палиндромом */
 
-const text = 'Лёша на полке клопа нашёл';
-const str = text.replaceAll(' ', '') .toLowerCase();
+//const text = 'Лёша на полке клопа нашёл';
+const isPalindrome = (value) => {
+  const normalized = value.replaceAll(' ', '') .toLowerCase();
+  let inverse = '';
+  for(let i = normalized.length - 1; i >= 0; i--) {
+    inverse += normalized[i] ;
+  }
+  return inverse === normalized;
+};
+//console.log(isPalindrome('1Лёша на полке клопа нашёл'));
 
-let nevStr = '';
 
-for(let i = str.length - 1; i >= 0; i--) {
-  nevStr += str[i] ;
-}
+/* дополнительное задание */
 
-const detPalindrome = () =>(nevStr === str);
-
-detPalindrome();
+const getNumbers = (value) => {
+  const preparedValue = String(value);
+  let result = '';
+  for (let i = 0; i < preparedValue.length; i++) {
+    if(!Number.isNaN(Number.parseInt(preparedValue[i], 10))) {
+      result += preparedValue[i];
+    }
+  }
+  return Number.parseInt(result, 10);
+};
+//console.log(getNumbers('1 кефир, 0.5 батона'));
