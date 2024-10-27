99a6b5b4e602b282ac62bf0460c8a0774d961eba
Владимир <10nva63@gmail.com>
2024-10-27 13:37:30 +0300

    Завершение ДЗ-4

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+const commentText = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент.'];
+
 const names = ['Николай', 'Таня', 'Сургей', 'Оля', 'Иван', 'Егор'];
 
 const getRandomNumber = (a, b) => {
@@ -7,10 +9,15 @@ const getRandomNumber = (a, b) => {
   return Math.floor(result);
 };
 
+const createMessage = () =>
+  Array.from({ length: getRandomNumber(1, 2) }, () =>
+    commentText[getRandomNumber(0, commentText.length - 1)]
+  ).join(' ');
+
 const createComment = (index) => ({
-  id: index , //getRandomNumber(0, 30)
+  id: index,
   avatar: `img/avatar-${index}.svg`,
-  message: 'В целом всё неплохо. Но не всё.',
+  message: createMessage(),
   name: names[getRandomNumber(0, 5)],
 });
 
@@ -20,11 +27,14 @@ const createFoto = (index) => ({
   url: `photos/${index}.jpg`,
   description: 'описание фотогафии',
   likes: getRandomNumber(15, 200) ,
-  comments: Array.from({ length: getRandomNumber(0, 30) }, createComment)
+  comments: Array.from(
+    { length: getRandomNumber(0, 6) },
+    (_, commentIndex) => createComment(commentIndex + 1)
+  )
 });
 
 const createFotos = () =>
-  Array.from({ length: 25 }, (_, createIndex) =>
-    createFoto(createIndex + 1)
+  Array.from(
+    { length: 25 }, (_, createIndex) => createFoto(createIndex + 1)
   );
-console.log(createFotos());
+createFotos();
