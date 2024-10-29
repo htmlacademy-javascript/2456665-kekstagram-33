85b76a89f9cc0d896ebda86fbb4a816dfe6602f7
Владимир <10nva63@gmail.com>
2024-10-29 09:53:45 +0300

    Доработка замечений к ДЗ_4

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
-const commentText = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент.'];
+const PHOTOS_COUNT = 25;
+const MIN_LIKES = 15;
+const MAX_LIKES = 200;
 
-const names = ['Николай', 'Таня', 'Сургей', 'Оля', 'Иван', 'Егор'];
+const DESCRIPTIONS = ['Утро', 'Вечер', 'Котик' ];
+
+const COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент.'];
+
+const NAMES = ['Николай', 'Таня', 'Сургей', 'Оля', 'Иван', 'Егор'];
 
 const getRandomNumber = (a, b) => {
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
@@ -9,32 +15,37 @@ const getRandomNumber = (a, b) => {
   return Math.floor(result);
 };
 
+const getRandomArrayElement = (data) => data[getRandomNumber(0, data.length - 1)];
+
 const createMessage = () =>
   Array.from({ length: getRandomNumber(1, 2) }, () =>
-    commentText[getRandomNumber(0, commentText.length - 1)]
+    COMMENTS[getRandomNumber(0, COMMENTS.length - 1)]
   ).join(' ');
 
 const createComment = (index) => ({
   id: index,
   avatar: `img/avatar-${index}.svg`,
   message: createMessage(),
-  name: names[getRandomNumber(0, 5)],
+  name: getRandomArrayElement(NAMES),
 });
 
 
 const createFoto = (index) => ({
   id: index,
   url: `photos/${index}.jpg`,
-  description: 'описание фотогафии',
-  likes: getRandomNumber(15, 200) ,
+  description: getRandomArrayElement(DESCRIPTIONS),
+  likes: getRandomNumber(MIN_LIKES, MAX_LIKES),
   comments: Array.from(
-    { length: getRandomNumber(0, 6) },
+    { length: getRandomNumber(0, NAMES.length - 1) },
     (_, commentIndex) => createComment(commentIndex + 1)
   )
 });
 
 const createFotos = () =>
   Array.from(
-    { length: 25 }, (_, createIndex) => createFoto(createIndex + 1)
+    { length: PHOTOS_COUNT }, (_, createIndex) => createFoto(createIndex + 1)
   );
-createFotos();
+
+console.log(createFotos());
+
+
